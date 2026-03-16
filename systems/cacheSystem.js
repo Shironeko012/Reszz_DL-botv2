6e4f5d3d03c5d0dae6a612dee335717611091ac1
Shironeko012 <153088951+Shironeko012@users.noreply.github.com>
2026-03-16 19:24:10 +0700

    Update cacheSystem.js

--- systems/cacheSystem.js
+++ systems/cacheSystem.js
@@ -1,7 +1,8 @@
 const logger = require("../utils/logger")
 const fileManager = require("../utils/fileManager")
 
-const CACHE_TTL = 24*60*60*1000
+const CACHE_TTL = 24 * 60 * 60 * 1000
+const MAX_CACHE = 500
 
 const cache = new Map()
 
@@ -13,10 +14,9 @@ if(!item) return null
 
 const now = Date.now()
 
-if(now-item.time > CACHE_TTL){
+if(now - item.time > CACHE_TTL){
 
 cache.delete(url)
-
 return null
 
 }
@@ -24,7 +24,6 @@ return null
 if(!fileManager.fileExists(item.file)){
 
 cache.delete(url)
-
 return null
 
 }
@@ -34,6 +33,24 @@ return item.file
 }
 
 function set(url,file){
+
+if(!fileManager.fileExists(file)){
+return
+}
+
+/*
+Limit cache size
+*/
+
+if(cache.size >= MAX_CACHE){
+
+const firstKey = cache.keys().next().value
+
+if(firstKey){
+cache.delete(firstKey)
+}
+
+}
 
 cache.set(url,{
 file,
@@ -46,21 +63,46 @@ function cleanup(){
 
 const now = Date.now()
 
-for(const [url,data] of cache.entries()){
+for(const [url,data] of cache){
 
-if(now-data.time > CACHE_TTL){
-
+if(now - data.time > CACHE_TTL){
 cache.delete(url)
+}
+
+}
+
+/*
+Hard limit protection
+*/
+
+if(cache.size > MAX_CACHE){
+
+const keys = cache.keys()
+
+for(let i=0;i<100;i++){
+
+const k = keys.next().value
+if(!k) break
+
+cache.delete(k)
 
 }
 
 }
 
+logger.info("CACHE_CLEANUP",{
+size:cache.size
+})
+
 }
+
+/*
+Cleanup every 10 minutes
+*/
 
 setInterval(cleanup,600000)
 
-module.exports={
+module.exports = {
 get,
 set
 }
